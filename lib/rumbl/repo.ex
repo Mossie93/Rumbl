35e294ead1638ca5cd2ffc3fb1592c0e4c7742cb
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-06-27 19:10:23 +0200

    Add #all method to lib/rumbl/repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,15 @@
 defmodule Rumbl.Repo do
+  @moduledoc """
+  In-memory repository
+  """
+
   use Ecto.Repo, otp_app: :rumbl
+
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: 1, name: "Dosia", username: "Dosian", password: "password123"},
+      %Rumbl.User{id: 2, name: "Zosia", username: "Zosian", password: "password456"},
+      %Rumbl.User{id: 3, name: "Gosia", username: "Gosian", password: "password789"}
+    ]
+  end
 end
