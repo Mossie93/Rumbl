1e1d119f1993956f394448ef1dffe777840056cc
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-06-29 22:49:59 +0200

    Use Ecto.Repo in Rumbl.Repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,28 +1,3 @@
 defmodule Rumbl.Repo do
-  @moduledoc """
-  In-memory repository
-  """
-  
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: 1, name: "Dosia", username: "Dosian", password: "password123"},
-      %Rumbl.User{id: 2, name: "Zosia", username: "Zosian", password: "password456"},
-      %Rumbl.User{id: 3, name: "Gosia", username: "Gosian", password: "password789"}
-    ]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id
-    end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn{key, val} ->
-        Map.get(map, key) == val
-      end)
-    end
-  end
+  use Ecto.Repo, otp_app: Rumbl
 end
