23606ae126344fce6a633f6d72c51c3a3748c383
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-07-03 17:56:23 +0200

    Add User#registration_changeset method

--- web/models/user.ex
+++ web/models/user.ex
@@ -17,4 +17,12 @@ defmodule Rumbl.User do
     |> unique_constraint(:username)
     |> validate_length(:username, min: 1, max: 20)
   end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password))
+    |> validate_length(:password, min: 6, max: 200)
+    |> put_pass_hash()
+  end
 end
