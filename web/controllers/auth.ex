f9210522ec132df4c205afef0bbe02ec2851b442
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-07-24 02:25:17 +0200

    Change Rumbl.Auth plug to assing user_token

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -16,6 +16,7 @@ defmodule Rumbl.Auth do
       user = conn.assigns[:current_user] ->
         conn
       user = user_id && repo.get(Rumbl.User, user_id) ->
+        put_current_user(conn, user)
         assign(conn, :current_user, user)
       true ->
         assign(conn, :current_user, nil)
@@ -24,7 +25,7 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -57,5 +58,13 @@ defmodule Rumbl.Auth do
       |> redirect(to: Helpers.page_path(conn, :index))
       |> halt()
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
