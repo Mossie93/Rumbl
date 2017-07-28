d6afb503d2522e534ad98d08b109c6c3adc2a35b
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-07-29 01:51:55 +0200

    Simulate Rumbl.Counter failure

--- lib/rumbl/counter.ex
+++ lib/rumbl/counter.ex
@@ -20,6 +20,8 @@ defmodule Rumbl.Counter do
 
   def handle_call(:val, _from, val), do: {:reply, val, val}
 
+  def handle_info(:tick, val) when val <=0, do: raise "boom!"
+
   def handle_info(:tick, val) do
     IO.puts "tick #{val}"
     Process.send_after(self, :tick, 1000)
